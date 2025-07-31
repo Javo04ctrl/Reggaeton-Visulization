02752c3db55deb9d879cd15f87ab83d2e669c357
Javo04ctrl <javichente6@gmail.com>
2025-07-31 00:58:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+import { Sigma } from "https://cdn.jsdelivr.net/npm/sigma@2.4.0/build/sigma.es.js";
+import { Graph } from "https://cdn.jsdelivr.net/npm/graphology@0.25.4/dist/graphology.esm.min.js";
+import { parse } from "https://cdn.jsdelivr.net/npm/graphology-gexf@0.13.2/browser/graphology-gexf.esm.min.js";
+
 fetch("graph.gexf")
   .then(res => res.text())
   .then(gexfString => {
-    const graph = window.graphologyGexf.parse(window.graphology, gexfString);
-    const renderer = new window.sigma.Sigma(graph, document.getElementById("container"));
+    const graph = parse(Graph, gexfString);
+    const renderer = new Sigma(graph, document.getElementById("container"));
   })
-  .catch(err => {
-    console.error("Error cargando el grafo:", err);
-  });
+  .catch(err => console.error("Error al cargar GEXF:", err));
